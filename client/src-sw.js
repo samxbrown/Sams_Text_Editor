4978c196b11c1e331d16b7945a591e687e423ce2
samxbrown <135293566+samxbrown@users.noreply.github.com>
2023-11-12 07:49:03 -0800

    updated client package.json and webpack.config file

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,12 +1,11 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
-
 precacheAndRoute(self.__WB_MANIFEST);
-
+// Set up page cache
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
@@ -18,13 +17,20 @@ const pageCache = new CacheFirst({
     }),
   ],
 });
-
 warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
 });
-
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-
-// TODO: Implement asset caching
-registerRoute();
+// Set up asset cache
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
